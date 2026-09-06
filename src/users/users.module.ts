659d90af2517e5b64b44e00c4a1b493aa6188462
jana237-cmr <babernaatz@gmail.com>
2026-09-06 09:02:22 +0100

    Create UsersModule with TypeOrm integration

---
+++ src/users/users.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { UsersService, UsersController } from './users.service';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([User])],
+  providers: [UsersService],
+  controllers: [UsersController],
+})
+export class UsersModule {}
