fe462116b2f6facaba7d8ba9be02fe46fc926221
jana237-cmr <babernaatz@gmail.com>
2026-09-06 08:57:42 +0100

    Add Wallet and WalletTransaction to AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../users/user.entity';
+import { Wallet } from '../wallet/wallet.entity';
+import { WalletTransaction } from '../wallet/wallet-transaction.entity';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([User]),
+    TypeOrmModule.forFeature([User, Wallet, WalletTransaction]),
     JwtModule.register({
       secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '7d' },
