a9ac25bacdaa229159bbe63362bf3bd73021486b
jana237-cmr <babernaatz@gmail.com>
2026-09-09 12:53:33 +0100

    Add files via upload

--- src/bookings/bookings.module.ts
+++ src/bookings/bookings.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Booking, BookingsService, BookingsController } from './bookings.service';
+import { User } from '../users/user.entity';
 import { WalletModule } from '../wallet/wallet.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Booking]), WalletModule],
+  imports: [TypeOrmModule.forFeature([Booking, User]), WalletModule],
   providers: [BookingsService],
   controllers: [BookingsController],
 })
