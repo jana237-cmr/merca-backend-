2a64b2420610a5440b476160295aebdd92db030e
jana237-cmr <babernaatz@gmail.com>
2026-09-10 00:49:59 +0100

    Add GeocodingModule to UsersModule imports

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { UsersService, UsersController } from './users.service';
+import { GeocodingModule } from '../geocoding/geocoding.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), GeocodingModule],
   providers: [UsersService],
   controllers: [UsersController],
 })
